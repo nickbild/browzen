83a6d29e403435dadee0b246584aaef8b47cc823
Nick Bild <nick.bild@gmail.com>
2021-02-05 15:19:46 -0500

    Refactor.

--- classify_emotion.py
+++ classify_emotion.py
@@ -12,9 +12,11 @@ import sqlite3
 import time
 
 
+class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 cut_size = 44
 
 
+# Create SQLite3 DB connection.
 def create_connection(db_file):
     conn = None
     try:
@@ -31,7 +33,7 @@ transform_test = transforms.Compose([
 ])
 
 
-
+# Load pre-trained model.
 net = VGG('VGG19')
 checkpoint = torch.load(os.path.join('FER2013_VGG19', 'PrivateTest_model.t7'))
 net.load_state_dict(checkpoint['net'])
@@ -39,49 +41,46 @@ net.cuda()
 net.eval()
 
 
+# Initialize camera and database connection.
 cap = cv2.VideoCapture(0)
+conn = create_connection('emotional_states.db')
 
-conn = create_connection('emotion_correlation.db')
 
 while True:
+    # Capture image from webcam.
     ret, frame = cap.read()
     cv2.imwrite('current.jpg', frame)
-    
+
+    # Transform image.
     raw_img = Image.open('current.jpg')
     gray = raw_img.convert('L')
     gray = gray.resize((48,48))
-
-    gray.save('current_final.jpg')
-
+    # gray.save('current_final.jpg')
     gray = np.array(gray)
     gray = gray.astype(np.uint8)
-
     img = gray[:, :, np.newaxis]
 
+    # Prepare image data for inference.
     img = np.concatenate((img, img, img), axis=2)
     img = Image.fromarray(img)
     inputs = transform_test(img)
-
-    class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
-
     ncrops, c, h, w = np.shape(inputs)
-
     inputs = inputs.view(-1, c, h, w)
     inputs = inputs.cuda()
     inputs = Variable(inputs, volatile=True)
+
+    # Run inference.
     outputs = net(inputs)
-
     outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
-
     score = F.softmax(outputs_avg)
     _, predicted = torch.max(outputs_avg.data, 0)
 
     print("Emotion: {0}".format(class_names[int(predicted.cpu().numpy())]))
 
+    # Save emotion data in database.
     sql = "INSERT INTO emotions(emotion) VALUES({0})".format(int(predicted.cpu().numpy()))
     cur = conn.cursor()
     cur.execute(sql)
     conn.commit()
 
     time.sleep(1)
-
